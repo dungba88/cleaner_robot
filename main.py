6bfd4231cff656b8d53c04971ee9a05614e8922d
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-10-07 14:05:00 +0800

    compare both algorithm

--- main.py
+++ main.py
@@ -31,23 +31,28 @@ def random_matrix(no_rows, no_cols, no_obs):
     return matrix, start_position
 
 def main():
-    no_rows = 4
-    no_cols = 3
-    no_obs = 2
-    no_matrix = 1
+    no_rows = 10
+    no_cols = 9
+    no_obs = 10
+    no_matrix = 10
 
-    total_elapsed = 0
-    total_steps = 0
-    total_turns = 0
+    total_elapsed_bfs = 0
+    total_steps_bfs = 0
+    total_turns_bfs = 0
+
+    total_elapsed_dfs = 0
+    total_steps_dfs = 0
+    total_turns_dfs = 0
 
     for i in range(no_matrix):
         matrix, start_position = random_matrix(no_rows, no_cols, no_obs)
         start_direction = random.randint(0, 3)
+
+        # run with dfs
         robot = Robot(matrix, start_position, start_direction)
+        robot.log()
         sweeper = DFSSweeper(robot)
         sweeper.loggable = False
-
-        robot.log()
 
         import time
 
@@ -55,18 +60,40 @@ def main():
         sweeper.sweep()
         elapsed = time.time() - start
 
-        total_elapsed += elapsed
-        total_steps += robot.move_count
-        total_turns += robot.turn_count
+        total_elapsed_dfs += elapsed
+        total_steps_dfs += robot.move_count
+        total_turns_dfs += robot.turn_count
 
-        print('steps taken: %d, turns taken: %d, time taken: %.2fms'
+        print('steps taken by dfs: %d, turns taken: %d, time taken: %.2fms'
+              % (robot.move_count, robot.turn_count, elapsed * 1000))
+
+        # run with bfs
+        robot = Robot(matrix, start_position, start_direction)
+        robot.log()
+        sweeper = Sweeper(robot)
+        sweeper.loggable = False
+
+        import time
+
+        start = time.time()
+        sweeper.sweep()
+        elapsed = time.time() - start
+
+        total_elapsed_bfs += elapsed
+        total_steps_bfs += robot.move_count
+        total_turns_bfs += robot.turn_count
+
+        print('steps taken by bfs: %d, turns taken: %d, time taken: %.2fms'
               % (robot.move_count, robot.turn_count, elapsed * 1000))
 
         # sweeper.print_map()
         robot.log()
 
-    print('average steps taken: %d, turns taken: %d, time taken: %.2fms'
-         % (int(total_steps / no_matrix), int(total_turns / no_matrix), total_elapsed * 1000 / no_matrix))
+    print('DFS: average steps taken: %d, turns taken: %d, time taken: %.2fms'
+         % (int(total_steps_dfs / no_matrix), int(total_turns_dfs / no_matrix), total_elapsed_dfs * 1000 / no_matrix))
+
+    print('BFS: average steps taken: %d, turns taken: %d, time taken: %.2fms'
+         % (int(total_steps_bfs / no_matrix), int(total_turns_bfs / no_matrix), total_elapsed_bfs * 1000 / no_matrix))
 
 if __name__ == '__main__':
     main()

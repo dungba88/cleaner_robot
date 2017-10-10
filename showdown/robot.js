5c37f55a6d10b428d9af7bf676df3703a6d55b6c
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-10-11 05:17:30 +0800

    fix bug

--- showdown/robot.js
+++ showdown/robot.js
@@ -30,7 +30,7 @@ class Application {
 
         for(let i = 0; i < matrix.length; i++) {
             for(let j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] == 1) {
+                if (matrix[i][j] != 0) {
                     ctx.fillRect(j * col_width, i * row_height, col_width, row_height);
                 }
             }
